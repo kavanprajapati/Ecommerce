23d617de1437f0ea166b20481c5b447dd51a6507
Kavan Prajapati <kavanprajapati9239@gmail.com>
2021-01-23 12:51:29 +0530

    constants file created

---
+++ my_ecom/config/constants.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'SITE_NAME'              => 'Ecommerce',
+    'SITE_EMAIL'             => 'myecom@gmail.com',
+    'SITE_LOGO'              => '',
+    'VALIDATION_MESSAGE'     => ''
+];
